597c3c592a7c414ff42f44e8aafd7b80a2b75466
Shu <controvictor@gmail.com>
2022-10-31 20:00:28 -0300

    Deleting users with DELETE

--- src/main/java/com/chaldeas/workshopmongo/services/UserService.java
+++ src/main/java/com/chaldeas/workshopmongo/services/UserService.java
@@ -25,13 +25,18 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
-	
+
 }
