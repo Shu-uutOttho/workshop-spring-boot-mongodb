62e90ffde9a1cd86413438962a63d64f842fc354
Shu <controvictor@gmail.com>
2022-11-01 17:52:39 -0300

    Get post by Id

---
+++ src/main/java/com/chaldeas/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.chaldeas.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.chaldeas.workshopmongo.domain.Post;
+import com.chaldeas.workshopmongo.repository.PostRepository;
+import com.chaldeas.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+}
