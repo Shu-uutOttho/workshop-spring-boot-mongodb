39c6a332516722b213d24ffa07bd7e8df3533d44
Shu <controvictor@gmail.com>
2022-11-03 09:50:52 -0300

    Simple query with Spring Data @Query

--- src/main/java/com/chaldeas/workshopmongo/resources/PostResource.java
+++ src/main/java/com/chaldeas/workshopmongo/resources/PostResource.java
@@ -35,9 +35,9 @@ public class PostResource {
 	}
 
 	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
-	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = " ") String text) {
 		text = URL.decodeParam(text);
-		List<Post> list = service.findByTitle(text);
+		List<Post> list = service.searchingTitle(text);
 		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/chaldeas/workshopmongo/services/PostService.java
+++ src/main/java/com/chaldeas/workshopmongo/services/PostService.java
@@ -27,8 +27,8 @@ public class PostService {
 	}
 	
 	
-	public List<Post> findByTitle(String text) {
-		return repo.findByTitleContainingIgnoreCase(text);
+	public List<Post> searchingTitle(String text) {
+		return repo.searchTitle(text);
 	}
 
 }
